ba0c1d577c639efa7db517e26bf1c3e0b08a1aee
Mister Dumbo <110036209+MrDumbo17@users.noreply.github.com>
2023-02-01 17:16:13 -0300

    teste

---
+++ costs/src/App.js
@@ -0,0 +1,36 @@
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import Container from './components/layouts/Container';
+import NavBar from './components/layouts/NavBar';
+import Footer from './components/layouts/Footer';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Container customClass='min-height'>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/projects/' element={<Projects/>} />
+            <Route path='/company' element={<Company/>} />
+            <Route path='/contact' element={<Contact/> }/>
+            <Route path='/newproject' element={<NewProject/>} />
+          </Routes>
+        </Container>
+      
+      </Router>
+      
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
